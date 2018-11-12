5400d2d44ef5129f9b15fbc63669823d85fd5dcf
callmabing <callmabing@163.com>
2018-11-12 22:55:39 +0800

    input动画

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import { 
     HeaderWrapper,
     Logo,
@@ -6,10 +7,17 @@ import {
     NavItem,
     NavSearch,
     Addition,
-    Button
+    Button,
+    SearchWrapper,
 } from './style'
 
 class Header extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            focused: false
+        }
+    }
     render() {
         return (
             <HeaderWrapper>
@@ -18,15 +26,42 @@ class Header extends Component{
                     <NavItem className='left active'>首页</NavItem>
                     <NavItem className='left'>下载App</NavItem>
                     <NavItem className='right'>登陆</NavItem>
-                    <NavItem className='right'>Aa</NavItem>
-                    <NavSearch></NavSearch>
+                    <NavItem className='right'>
+                        <i className="iconfont">&#xe636;</i>
+                    </NavItem>
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={this.state.focused}
+                            timeout={200}
+                            classNames="slide"
+                        >
+                            <NavSearch
+                                className={this.state.focused ? 'focused' : ''}
+                                onFocus={this.handleInputFocus.bind(this)}
+                                onBlur={this.handleInputBlur.bind(this)}
+                            ></NavSearch>
+                        </CSSTransition>
+                        <i className={this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe653;</i>
+                    </SearchWrapper>
                 </Nav>
                 <Addition>
-                    <Button className='writebtn'>写文章</Button>
-                    <Button className="reg ">注册</Button>
+                    <Button className='writebtn'>
+                        <i className="iconfont">&#xe60b;</i>写文章
+                    </Button>
+                    <Button className="reg">注册</Button>
                 </Addition>
             </HeaderWrapper>
         )
+    }
+    handleInputFocus () {
+        this.setState({
+            focused: true
+        })
+    }
+    handleInputBlur () {
+        this.setState({
+            focused: false
+        })
     }
 }
 
