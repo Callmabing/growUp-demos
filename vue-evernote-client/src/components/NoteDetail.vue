5405661181d19331250c4374ce8d4b5813dac169
callmabing <callmabing@163.com>
2019-01-02 23:45:46 +0800

    引入element-ui

--- vue-evernote-client/src/components/NoteDetail.vue
+++ vue-evernote-client/src/components/NoteDetail.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="note-detail">
     <h1>{{msg}} : {{ $route.params.noteId }}</h1>
+    <div>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </div>
   </div>
 </template>
 
 <script>
+import Auth from '@/apis/auth'
+
 export default {
   name: 'Login',
   data () {
     return {
       msg: '笔记本详情页'
     }
+  },
+  created() {
+    Auth.getInfo()
+      .then(res => {
+        if(!res.isLogin){
+          this.$router.push({ path: '/login' })
+        }
+      })
   }
 }
 </script>
